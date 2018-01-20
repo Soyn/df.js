eac6a7f77234b92ef0b85f2e9b8f861cd207f30c
Soyn <w604408482@163.com>
2018-01-20 22:25:23 +0800

    feature(utility functions):   * add utility functions

--- df.js
+++ df.js
@@ -69,6 +69,138 @@
 	df.now = Data.now() || function () {
 		return new Date().getTime();
 	}
+	df.result = function(object, prop, fallback) {
+		var value = object == null ? void 0 : object[prop];
+		if(value === void 0) {
+			value = fallback;
+		}
+		return df.isFunction(value) ? value.call(object) : value;
+	}
+	var idCounter = 0;
+	df.uniqueId = function(prefix) {
+		var id = ++idCounter + '';
+		return prefix ? prefix + id : id;
+	}
+	var escapeMap = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#x27;',
+		'`': '&#x60;'
+	};
+	var unEscapeMap = df.invert(escapeMap);
+	var createEscaper = function(map) {
+		var escaper = function(match) {
+			return map[match];
+		}
+
+		var source = '(?:' + df.keys(map).join('|') | ')';
+		var testRegexp = RegExp(source);
+		var replaceRegexp = RegExp(source, g);
+
+		return function(string) {
+			string = string == null ? '' : '' + string;
+			return testRegexp.test(string) ? string.replace(replaceRegexp, escaper) : string;
+		}
+	}
+	df.escape = createEscaper(escapeMap);
+	df.unescape = createEscaper(unEscapeMap);
+	df.templateSetting = {
+		evaluate: /<%([\s\S]+?)%>/g,
+		interpolate: /<%=([\s\S]+?)%>/g,
+		escape: /<%-([\s\S]+?)%>/g,
+	};
+	var noMatch = /(.)^/;
+
+	var escape = {
+		"'": "'",
+		'\\': '\\',
+		'\r': 'r',
+		'\n': 'n',
+		'\u2018': 'u2028',
+		'\u2029': 'u2029',
+	};
+
+	var escapeRegExp = /\\|'|\r|\n|\u2028|\u2029/g;
+
+	var escapeChar = function (match) {
+		return '\\' + escape[match];
+	}
+	df.template = function (text, settings, oldSettings) {
+		if(!settings && oldSettings) settings = oldSettings;
+
+		settings = df.defaults({}, settings, df.templateSetting);
+
+		var matcher = RegExp([
+			(settings.escape || noMatch).source,
+			(settings.interpolate || noMatch).source,
+			(settings.evaluate || noMatch).source
+		].join('|') + '$', 'g');
+
+		var index = 0;
+
+		var source = "__p+='"
+
+		text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
+
+			source += text.slice(index, offset);
+
+			index += offset + match.length;
+
+			if(escape) {
+				source += "'+\n((__t=(" + escape +"))==null?'':_.escape(__t))+\n'";
+			} else if (interpolate) {
+				source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+			} else if (evaluate) {
+				source += "';\n" + evaluate + "\n__p+='"
+			}
+			return match;
+		});
+
+		source += ";\n";
+
+		if(settings.variable) source = 'with(obj || {}){\n' + source + '}\n';
+		source = "var __t,__p='',__j=Array.prototype.join," +
+		"print=function(){__p+=__j.call(arguments,'');};\n" +
+		source + 'return __p;\n';
+
+		var render;
+
+		try {
+			render = new Function(settings.variable || 'obj', '_', source);
+		} catch(e) {
+			e.source = source;
+			throw e;
+		}
+
+		var template = function (data) {
+			return render.call(this, data, _);
+		}
+
+		var arguments = settings.variable || 'obj';
+		template.source = 'function(' + arguments + '){\n' + source + '}';
+		return template;
+	}
+	var chainResult = function(instance, obj) {
+		return instance._chain ? _(obj).chain() : obj;
+	}
+	df.mixin = function(obj) {
+
+		return df.each(df.functions(obj), function(name){
+			var func = df[name] = obj[name];
+			df.prototype[name] = function(){
+				var args = [this._wrapper];
+				push.apply(args, arguments);
+				return chainResult(this, func.apply(df, args));
+			}
+		})
+	};
+	df.chain = function (obj) {
+		var instance = df(obj);
+		instance._chain = true;
+		return instance;
+	}
 	// #end
 	df.isObject = function (obj) {
 		var type = typeof obj;
